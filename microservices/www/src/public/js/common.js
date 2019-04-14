ca6d5506b058cc7ca0a2871333494e7f1ee7d388
Justin Mitchell <justin@jmitch.com>
2019-04-14 19:12:55 -0400

    changed cluster name

--- microservices/www/src/public/js/common.js
+++ microservices/www/src/public/js/common.js
@@ -1,5 +1,5 @@
 var rootURL = window.location.protocol + "//" + window.location.host;
-var cluster_name = "flashiness71";
+var cluster_name = "reducible58";
 var auth_query_url = "https://auth." + cluster_name + ".hasura-app.io/v1";
 var data_query_url = "https://data." + cluster_name + ".hasura-app.io/v1/query";
 var app_url = "https://www." + cluster_name + ".hasura-app.io";
